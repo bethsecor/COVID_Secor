b75981e6901cd5598143d50686822478aa79b857
Beth Secor <elizabeth.a.secor@gmail.com>
2020-09-01 13:53:02 -0600

    Read in Census data, change CDC data to last 7 days

--- README.md
+++ README.md
@@ -4,8 +4,6 @@ The purpose of this project is to use data on COVID-19 cases from a few sources 
 
 ### Data Sources
 
-Last downloaded: August 31, 2020
-
-1. [CDC COVID Data Tracker Cases](https://covid.cdc.gov/covid-data-tracker/#cases): ./data/raw/CDC/total_cases_by_state_territory.csv
-2. [Census State Population Totals](https://www.census.gov/data/datasets/time-series/demo/popest/2010s-state-total.html): ./data/raw/Census/nst-est2019-alldata.csv (Population, Population Change, and Estimated Components of Population Change: April 1, 2010 to July 1, 2019)
-3. [The COVID Tracking Project](https://covidtracking.com/data): ./data/raw/COVID_tracking/all-states-history.csv
+1. [CDC COVID Data Tracker Cases in Last 7 days](https://covid.cdc.gov/covid-data-tracker/#cases): ./data/raw/CDC/cases_in_last_7_days_by_state_territory.csv, downloaded September 1, 2020
+2. [Census State Population Totals](https://www.census.gov/data/datasets/time-series/demo/popest/2010s-state-total.html): ./data/raw/Census/nst-est2019-alldata.csv, downloaded August 31, 2020 (Population, Population Change, and Estimated Components of Population Change: April 1, 2010 to July 1, 2019)
+3. [The COVID Tracking Project](https://covidtracking.com/data): ./data/raw/COVID_tracking/all-states-history.csv, downloaded August 31, 2020
--- code/clean_data.R
+++ code/clean_data.R
@@ -1,26 +1,37 @@
-# This code reads in csv data and cleans it
+# This code reads in csv data from the CDC, Census, and The COVID Tracking Project and cleans it to create two final datasets
 # Author: Beth Secor
 library(tidyverse)
 
-# Path to git repository:
+## Path to git repository:
 path <- "~/Documents/CDPHE/COVID_Secor"
 
-## Read in data:
-COVID_cases_CDC <- read.csv(paste(path,"/data/raw/CDC/total_cases_by_state_territory.csv", sep=""), skip=3, header=T)
-head(COVID_cases_CDC)
-summary(COVID_cases_CDC)
-# change confirmed and probable from character to numeric
-COVID_cases_CDC$Confirmed <- as.numeric(COVID_cases_CDC$Confirmed)
-COVID_cases_CDC$Probable <- as.numeric(COVID_cases_CDC$Probable)
+## Read in CDC data:
+# Last Downloaded: September 1, 2019
+COVID_cases_7_CDC <- read.csv(paste(path,"/data/raw/CDC/cases_in_last_7_days_by_state_territory.csv", sep=""), skip=3, header=T)
+head(COVID_cases_7_CDC)
+summary(COVID_cases_7_CDC)
+
 # note: New York does not include New York City cases
-COVID_cases_CDC$State <- ifelse(COVID_cases_CDC$State.Territory == "New York City", "New York", COVID_cases_CDC$State.Territory)
+COVID_cases_7_CDC$state <- ifelse(COVID_cases_7_CDC$State.Territory == "New York City", "New York", COVID_cases_7_CDC$State.Territory)
 
-# Remove Puerto Rico and outer islands and combine New York and NYC cases
-COVID_cases_CDC_state <- COVID_cases_CDC %>% filter(!(State.Territory %in% c("Puerto Rico","Guam","Virgin Islands","Northern Mariana Islands","American Samoa", "Federated States of Micronesia", "Palau", "Republic of Marshall Islands"))) %>% group_by(State) %>% summarize(st_cases = sum(Total.Cases), st_confirmed = sum(Confirmed), st_probable = sum(Probable)) %>% ungroup()
+# Remove DC, Puerto Rico, and outer islands and combine New York and NYC cases
+# question: Combine DC into Maryland or Virginia or leave it out?
+COVID_cases_7_CDC_state <- COVID_cases_7_CDC %>% filter(!(State.Territory %in% c("District of Columbia","Puerto Rico","Guam","Virgin Islands","Northern Mariana Islands","American Samoa", "Federated States of Micronesia", "Palau", "Republic of Marshall Islands"))) %>% group_by(state) %>% summarize(cases.7days = sum(Cases.in.Last.7.Days)) %>% ungroup()
 
-# Check if query is correct:
-summary(COVID_cases_CDC_state)
-COVID_cases_CDC[COVID_cases_CDC$State.Territory %in% c("New York City","New York"),]
-COVID_cases_CDC_state[COVID_cases_CDC_state$State %in% c("New York"),] #since New York does not track confirmed and probable, NYC's confirmed and probable would not be relevant at the state level.
+# Check if query is as expected:
+summary(COVID_cases_7_CDC_state)
+COVID_cases_7_CDC[COVID_cases_7_CDC$State.Territory %in% c("New York City","New York"),]
+COVID_cases_7_CDC_state[COVID_cases_7_CDC_state$state %in% c("New York"),]
 
+## Read in Census data:
+population_all <- read.csv(paste(path,"/data/raw/Census/nst-est2019-alldata.csv", sep=""), header=T)
+head(population_all)
+summary(population_all)
+# note: 2019 population estimates are as of July 1, 2019 (most recent available from Census)
+population_state_2019 <- population_all %>% filter(SUMLEV == 40 & !(NAME %in% c("District of Columbia","Puerto Rico"))) %>% select(NAME, CENSUS2010POP, POPESTIMATE2019) %>% rename(state = NAME)
+summary(population_state_2019)
 
+## Merge COVID-19 case counts (last 7 days) with population estimates for 2019 and create incidence per 100,000:
+COVID_cases_7_pop_state <- full_join(COVID_cases_7_CDC_state, population_state_2019, by="state") %>% mutate(incidence_7days = cases.7days / POPESTIMATE2019 * 100000)
+save(COVID_cases_7_pop_state, file = paste(path,"/data/COVID_7days_byState.Rda",sep=""))
+write.csv(COVID_cases_7_pop_state, file = paste(path,"/data/COVID_7days_byState.csv",sep=""))
